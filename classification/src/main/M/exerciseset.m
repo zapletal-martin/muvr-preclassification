53786a3535eeb6d78eb9749a5e0c51a5f342d088
Jan Machacek <jan.machacek@gmail.com>
2015-03-22 18:05:49 +0000

    Analysis

--- classification/src/main/M/exerciseset.m
+++ classification/src/main/M/exerciseset.m
@@ -6,15 +6,23 @@
 % To see the effect of non-exercise, remove the filtering on the .timestamp
 
 M = readtable('all_4.csv');
-rows = strcmp(M.location, 'wrist.0') & M.timestamp > 800 & M.timestamp < 2000;
-% ``xs`` are just the [timestamp, x] points from M
-xs   = table2array(M(rows,5));
+rows = strcmp(M.location, 'wrist.0') & M.timestamp < 800 ;%& M.timestamp < 1400;
+
 time = table2array(M(rows,1));
-plot(time, xs);
+ads  = table2array(M(rows,[5, 6, 7]));
+subplot(2, 1, 1);
+plot(time, ads);
 
 %% Use the sigfft function
 % Analyse the signal
-[freq, period, power] = sigfft(xs);
+[freqx, periodx, powerx] = sigfft(ads(:,1));
+[freqy, periody, powery] = sigfft(ads(:,2));
+[freqz, periodz, powerz] = sigfft(ads(:,3));
 
-plot(period, power);
-title(['Period = ', num2str(freq)]);
+subplot(2, 1, 2);
+hold on;
+plot(periodx, powerx);
+plot(periody, powery);
+plot(periodz, powerz);
+title(['Period = ', num2str([freqx, freqy, freqz])]);
+hold off;
