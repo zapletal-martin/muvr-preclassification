f10eb1b9299626eaf125bf4cfabfa0232933671d
Jan Machacek <jan.machacek@gmail.com>
2015-03-26 08:42:37 +0000

    Nail, meet hammer. Performance & memory leak tests.

---
+++ core/test/cc/exercise_decider_perf_test.cc
@@ -0,0 +1,17 @@
+#include "sensordata.h"
+#include "test_data.h"
+#include <gtest/gtest.h>
+
+using namespace muvr;
+
+class exercise_decider_perf_test : public testing::Test {
+protected:
+    exercise_decider decider;
+};
+
+TEST_F(exercise_decider_perf_test, trivial_exercise) {
+    auto sd = test_data_generator(accelerometer).with_noise(10).sin(10, 100, Scalar(1000, 1000, 1000));
+    for (int i = 0; i < 100000; ++i) {
+        EXPECT_EQ(exercise_decider::exercise_result::yes, decider.has_exercise(sd));
+    }
+}
