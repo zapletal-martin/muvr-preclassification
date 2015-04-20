c235ed86029ca342d50223bf77c919ea61c81edd
martinzapletal <zapletal-martin@email.cz>
2015-04-20 14:30:30 +0200

    Added more tests

--- core/test/cc/sampler_test.cc
+++ core/test/cc/sampler_test.cc
@@ -9,29 +9,55 @@ class sampler_test : public testing::Test {
 
 };
 
-TEST_F(sampler_test, input_frequency_same_as_output) {
-    auto movement_data = raw_sensor_data_loader("all_4.csv").from_type(accelerometer).drop_zeros().from_sensor("wrist.0").first_value(574).max_values(100).load();
-
+std::vector<raw_sensor_data> prepare_test_data() {
     Mat f1(1, 1, CV_8UC3, Scalar(10, 10, 10));
-    f1.push_back(Mat (1, 1, CV_8UC3, Scalar(20, 20, 20)));
-    f1.push_back(Mat (1, 1, CV_8UC3, Scalar(30, 30, 30)));
-    f1.push_back(Mat (1, 1, CV_8UC3, Scalar(40, 40, 40)));
-    f1.push_back(Mat(1, 1, CV_8UC3, Scalar(50, 50, 50)));
+    f1.push_back(Mat (1, 1, CV_8UC3, cvScalar(20, 20, 20)));
+    f1.push_back(Mat (1, 1, CV_8UC3, cvScalar(30, 30, 30)));
+    f1.push_back(Mat (1, 1, CV_8UC3, cvScalar(40, 40, 40)));
+    f1.push_back(Mat(1, 1, CV_8UC3, cvScalar(50, 50, 50)));
 
     Mat s1(1, 1, CV_8UC3, Scalar(50, 50, 50));
-    s1.push_back(Mat (1, 1, CV_8UC3, Scalar(40, 40, 40)));
-    s1.push_back(Mat (1, 1, CV_8UC3, Scalar(30, 30, 30)));
-    s1.push_back(Mat (1, 1, CV_8UC3, Scalar(20, 20, 20)));
-    s1.push_back(Mat (1, 1, CV_8UC3, Scalar(10, 10, 10)));
+    s1.push_back(Mat (1, 1, CV_8UC3, cvScalar(40, 40, 40)));
+    s1.push_back(Mat (1, 1, CV_8UC3, cvScalar(30, 30, 30)));
+    s1.push_back(Mat (1, 1, CV_8UC3, cvScalar(20, 20, 20)));
+    s1.push_back(Mat (1, 1, CV_8UC3, cvScalar(10, 10, 10)));
 
-    std::vector<raw_sensor_data> movement_data_vector {
+    return std::vector<raw_sensor_data> {
         raw_sensor_data(f1, accelerometer, 5, 0, 1000),
         raw_sensor_data(s1, accelerometer, 5, 0, 1000)
     };
+}
+
+TEST_F(sampler_test, input_frequency_same_as_output) {
+    std::vector<raw_sensor_data> movement_data_vector  = prepare_test_data();
 
     auto result = resample(movement_data_vector, 5);
 
     EXPECT_EQ(2000, result.duration);
     EXPECT_EQ(5, result.samples_per_second);
     EXPECT_EQ(10, result.data.rows);
+}
+
+TEST_F(sampler_test, output_frequency_higher) {
+    std::vector<raw_sensor_data> movement_data_vector  = prepare_test_data();
+
+    auto result = resample(movement_data_vector, 10);
+
+    EXPECT_EQ(2000, result.duration);
+    EXPECT_EQ(10, result.samples_per_second);
+    EXPECT_EQ(20, result.data.rows);
+}
+
+TEST_F(sampler_test, output_frequency_lower) {
+    std::vector<raw_sensor_data> movement_data_vector = prepare_test_data();
+
+    auto result = resample(movement_data_vector, 2);
+
+    EXPECT_EQ(2000, result.duration);
+    EXPECT_EQ(2, result.samples_per_second);
+    EXPECT_EQ(4, result.data.rows);
+}
+
+TEST_F(sampler_test, the_inputs_have_different_frequencies) {
+    std::vector<raw_sensor_data> movement_data_vector = prepare_test_data();
 }
