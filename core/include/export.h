716391c64160465f105f1579af3f167b6673ac78
Jan Machacek <jan.machacek@gmail.com>
2015-04-26 16:28:39 +0100

    Export in JSON

--- core/include/export.h
+++ core/include/export.h
@@ -4,7 +4,11 @@
 #include "sensor_data.h"
 
 namespace muvr {
-
+    
+    ///
+    /// Exports the ``data`` to the given ``stream`` in a JSON format so that
+    /// it can be very easily received & interpeted by some server.
+    ///
     void export_data(std::ostream& stream, const fused_sensor_data& data);
 
 }
--- core/main/cc/export.cc
+++ core/main/cc/export.cc
@@ -3,6 +3,7 @@
 namespace muvr {
 
     void export_data(std::ostream& stream, const fused_sensor_data& data) {
+        /* CSV
         // mat[...],device_id,sensor_type,sensor_location,samples_per_second
         std::string tail = std::to_string(data.device_id) + "," +
                            std::to_string(data.sensor_type) + "," +
@@ -15,6 +16,43 @@ namespace muvr {
             }
             stream << tail << std::endl;
         }
+        */
+
+        /* JSON:
+            { 
+              "deviceId": $number,
+              "sensorType": $number,
+              "location": $number,
+              "samplesPerSecond": $number,
+              "data":[{"x": $number, "y": $number, "z": $number}, ... ]
+            }
+         
+         */
+        stream << "{\"deviceId\":" << std::to_string(data.device_id);
+        stream << ",\"sensorType\":" << std::to_string(data.sensor_type);
+        stream << ",\"sensorLocation\":" << std::to_string(data.sensor_location);
+        stream << ",\"samplesPerSecond\":" << std::to_string(data.samples_per_second);
+        stream << ",\"data\":[";
+        
+        const Mat &mat = data.data;
+        for (int i = 0; i < mat.rows; ++i) {
+            if (i > 0) stream << ",";
+            if (mat.cols == 3) {
+                int16_t x = mat.at<int16_t>(i, 0);
+                int16_t y = mat.at<int16_t>(i, 1);
+                int16_t z = mat.at<int16_t>(i, 2);
+                stream << "{\"x\":" << std::to_string(x)
+                       << ",\"y\":" << std::to_string(y)
+                       << ",\"z\":" << std::to_string(z)
+                       << "}";
+            } else if (mat.cols == 1) {
+                int16_t v = mat.at<int16_t>(i, 0);
+                stream << std::to_string(v);
+            } else {
+                throw std::runtime_error("Not 3D or 1D matrix.");
+            }
+        }
+        stream << "]}";
     }
 
 
