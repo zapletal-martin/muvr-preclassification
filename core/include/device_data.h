cf203eb14bcd28c438a1f974511261c2c7d8cfed
Jan Machacek <jan.machacek@gmail.com>
2015-05-02 20:01:59 +0100

    Feedback

--- core/include/device_data.h
+++ core/include/device_data.h
@@ -2,6 +2,7 @@
 #define _PRECLASSIFICATION_SENSOR_FORMATS_H_
 
 #include <stdint.h>
+#include <string.h>
 
 ///
 /// Every device must pack its data into the following header
@@ -34,5 +35,52 @@ typedef struct __attribute__((__packed__)) {
     int16_t z     : 13;
     int8_t  valid : 1;
 } device_data_threed;
+
+/// Repetition count
+typedef uint8_t repetitions_t;
+
+/// Intensity in 0..100
+typedef uint8_t intensity_t;
+
+/// Classification confidence in 0..100
+typedef uint8_t confidence_t;
+
+/// Weight in user units (converted on the mobile). Beyond mobile, everything is in the metric system
+typedef uint16_t weight_t;
+
+#define UNK_REPETITIONS 255
+#define UNK_INTENSITY 255
+#define UNK_WEIGHT 65535
+
+///
+/// This is the simple resistance exercise example
+///
+typedef struct __attribute__((__packed__)) {
+    char name[24];
+    confidence_t  confidence;       // 0..100
+    repetitions_t repetitions;      // 1..~50,  UNK_REPETITIONS for unknown
+    intensity_t   intensity;        // 1..100,  UNK_INTENSITY for unknown
+    weight_t      weight;           // 1..~500, UNK_WEIGHT for unknown
+} resistance_exercise_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+///
+/// Simple "constructor"
+///
+void mk_resistance_exercise(void *memory,
+                       const char *name, const confidence_t confidence, const repetitions_t repetitions, const intensity_t intensity, const weight_t weight) {
+    resistance_exercise_t *re = (resistance_exercise_t*)memory;
+    strncpy(re->name, name, 24);
+    re->confidence = confidence;
+    re->repetitions = repetitions;
+    re->intensity = intensity;
+    re->weight = weight;
+}
+#ifdef __cplusplus
+}
+#endif
 
 #endif //_PRECLASSIFICATION_SENSOR_FORMATS_H_
