1f990ea496be5b8299fb51d59e5c4d9fe2ad0e09
Jan Machacek <jan.machacek@gmail.com>
2015-05-03 16:08:51 +0100

    Encoder

--- core/include/device_data.h
+++ core/include/device_data.h
@@ -70,15 +70,7 @@ extern "C" {
 ///
 /// Simple "constructor"
 ///
-void mk_resistance_exercise(void *memory,
-                       const char *name, const confidence_t confidence, const repetitions_t repetitions, const intensity_t intensity, const weight_t weight) {
-    resistance_exercise_t *re = (resistance_exercise_t*)memory;
-    strncpy(re->name, name, 24);
-    re->confidence = confidence;
-    re->repetitions = repetitions;
-    re->intensity = intensity;
-    re->weight = weight;
-}
+void mk_resistance_exercise(void *memory, const char *name, const confidence_t confidence, const repetitions_t repetitions, const intensity_t intensity, const weight_t weight);
 #ifdef __cplusplus
 }
 #endif
