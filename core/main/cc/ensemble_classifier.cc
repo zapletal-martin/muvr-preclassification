c2f93752c8e3d4e90d70f2b4cfdde0dfd9159948
martinzapletal <zapletal-martin@email.cz>
2015-05-05 10:02:00 +0100

    Ensemble classifier

--- core/main/cc/ensemble_classifier.cc
+++ core/main/cc/ensemble_classifier.cc
@@ -3,12 +3,15 @@
 using namespace muvr;
 
 //TODO: Fix as soon as we have multiple classifiers working
+//TODO: This should return confidence ordered list of exercises build from votes from mutliple classifiers
 svm_classifier::classification_result vote(const std::vector<svm_classifier::classification_result> &results) {
     double max_confidence = 0;
     svm_classifier::classification_result max_result = results[0];
 
     for(int i = 0; i < results.size(); ++i) {
         if(results[i].exercises().size() > 0) {
+
+            std::cout << "exercise" << results[i].exercises()[0].exercise_name() << " with confidence " << results[i].exercises()[0].confidence() << std::endl;
             if(results[i].exercises()[0].confidence() > max_confidence) {
                 max_result = results[i];
             }
