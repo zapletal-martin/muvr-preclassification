8a8af45dca0986a2f3794b476ccf2e52e8369a8e
Jan Machacek <jan.machacek@gmail.com>
2015-05-24 18:26:56 +0100

    Generated charts

--- core/main/M/multiple.m
+++ core/main/M/multiple.m
@@ -1,7 +1,12 @@
-range = 22:27;
-dir = '/Users/janmachacek/Muvr/muvr-preclassification/training-data/jan-back-22-05/';
+range = 1:37;
+dir = '/Users/janmachacek/Muvr/muvr-preclassification/training-data/jan-shoulders-21-05/';
+
 for i = range
+    % Prepare file names
     fileName = strcat(strcat(dir, num2str(i)), '.csv');
+    imageName = strcat(strcat(dir, num2str(i)), '.png');
+    
+    % Load the CSV
     M = readtable(fileName);
     ads = table2array(M(:,[5,6,7]));
 
@@ -12,8 +17,11 @@ for i = range
     adsf = filter(b, a, ads);
     adsf = adsf(windowSize:end,:);
 
+    % Save the figure
     titleCell = M{1,1:2};
-    figure('name', strcat(strcat(titleCell{1}, ' '), titleCell{2}));
+    fig = figure('name', strcat(strcat(titleCell{1}, ' '), titleCell{2}));
     time = 1:length(adsf);
     plot(time, adsf);
+    print(fig, imageName, '-dpng');
+    close(fig);
 end
